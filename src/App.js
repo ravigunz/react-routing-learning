57c9254689b96c4742bbc5a94aac061b238212ac
Ravi <ravigunz@gmail.com>
2023-06-06 09:44:07 +0530

    set basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Book, Contact, Home, NewBook, NotFound, About } from "./pages/pages";
+import BookList from "./pages/BookList";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/newbook" element={<NewBook />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/pages.js
@@ -0,0 +1,7 @@
+export { default as Home } from './Home';
+export { default as Books } from './BookList';
+export { default as Book } from './Book';
+export { default as Contact } from './Contact';
+export { default as NewBook } from './NewBook';
+export { default as About } from './About';
+export { default as NotFound } from './NotFound';
